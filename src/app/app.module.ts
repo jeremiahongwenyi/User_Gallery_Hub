fb48952cea6e8614e45462fbb08dd6e36c84e779
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-01-28 15:48:30 +0300

    feat:add landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { FooterComponent } from './footer/footer.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LandingpageComponent,
+    FooterComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { User } from '../services/models/User';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss'
+})
+export class UserComponent implements OnInit{
+
+  constructor(private userService:UserService){}
+
+  allUsers:User[]=[];
+
+  ngOnInit(): void {
+      this.subscribeUser()
+  }
+
+subscribeUser(){
+   this.userService.fetchUsers().subscribe({
+    next:(resp)=>{
+      console.log(resp);
+      
+      this.allUsers=resp
+    },
+    error:(err)=>{
+
+    }
+   })
+}
+}
