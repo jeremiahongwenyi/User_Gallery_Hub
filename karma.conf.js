79906d44b0db9ab4b52861a967c3b18f0469d2a1
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-01-30 22:04:03 +0300

    chore: modified ci.yml file

--- karma.conf.js
+++ karma.conf.js
@@ -1,34 +1,54 @@
+// Import puppeteer for Chrome executable
 const puppeteer = require('puppeteer');
-
 process.env.CHROME_BIN = puppeteer.executablePath();
 
 module.exports = function(config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine'],
-    files: [],
-    exclude: [],
-    preprocessors: {},
-    reporters: ['progress'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],  // Added Angular framework
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false,  // leave Jasmine Spec Runner output visible
+      jasmine: {
+        timeoutInterval: 10000  // Increased timeout for CI environment
+      }
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false, // Set to false for CI
-    browsers: ['ChromeHeadlessNoSandbox'],
+    autoWatch: false,
+    browsers: ['ChromeHeadlessCI'],
     customLaunchers: {
-      ChromeHeadlessNoSandbox: {
+      ChromeHeadlessCI: {
         base: 'ChromeHeadless',
         flags: [
           '--no-sandbox',
           '--disable-gpu',
-          '--disable-software-rasterizer',
           '--disable-dev-shm-usage',
+          '--disable-web-security',
           '--remote-debugging-port=9222',
-          '--headless'
+          '--headless=new'  // Updated to use the new headless mode
         ]
       }
     },
-    singleRun: true, // Ensures Karma exits after tests finish
+    singleRun: true,
+    failOnEmptyTestSuite: false,
+    restartOnFileChange: false,
     concurrency: Infinity
   });
 };
