d83962d22cb5bf0c4eb265c908cf6455e3584a46
jeremiahongwenyi <jerrmiahongwenyi@gmail.com>
2025-01-31 08:55:04 +0300

    chore: modified environment.prod.ts file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
-      production: true,
-      apiUrl: 'undefined',
-      authUrl: 'undefined',
-      firebaseConfig: {
-        firebaseSignUpApi: 'undefined',
-        firebaseSignInApi: 'undefined',
-        firebaseApiKey: 'undefined',
-        firebaseDatabaseURL: 'undefined'
-      }
-    };
+  production: false,
+  apiUrl: 'https://jsonplaceholder.typicode.com',
+
+  firebaseConfig: {
+    firebaseApiKey: 'AIzaSyChWiDZzPAzAxQjCk102RSlImTtPG1PxNM',
+    firebaseDatabaseURL: 'https://authentication-23e9a-default-rtdb.firebaseio.com/',
+    firebaseSignInApi: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=',
+    firebaseSignUpApi: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key='
     
+  }
+};
+
